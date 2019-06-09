f56230e4719c72b8f29ab7ff84f74593589579f5
Mukesh Chaudhary <mukesh.chaudhary@getfareye.com>
2019-06-10 02:50:05 +0530

    Add Rspec

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -1,1 +1,13 @@
-console.log('👻');
+import 'babel-polyfill';
+import React from 'react';
+import { render } from 'react-dom';
+import Provider from '../components/Provider';
+import Library from '../components/Library';
+
+render(
+  <Provider>
+    <Library/>
+  </Provider>,
+  document.querySelector('#root')
+);
+
